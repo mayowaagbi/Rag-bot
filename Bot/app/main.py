886d48e204ba6d771b132e140193142f3d883920
mayowaagbi <agbimayowa3@gmail.com>
2025-05-28 05:39:55 +0100

    error fixes

--- Bot/app/main.py
+++ Bot/app/main.py
@@ -42,6 +42,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://localhost:5173",
         "http://127.0.0.1:3000",
+        "https://mayowas-rag-bot.vercel.app",
     ],  # Add your frontend URLs
     allow_credentials=True,
     allow_methods=["*"],
